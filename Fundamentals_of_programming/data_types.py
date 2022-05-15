57a9d3b0bcccae29fa02434ca608db97c6fd886d
Srijan <srijan.vaddadi@gmail.com>
2022-05-15 20:36:03 +0100

    data types

---
+++ Fundamentals_of_programming/data_types.py
@@ -0,0 +1,25 @@
+import datetime
+
+integer_variable = 5
+float_variable = 3.2
+boolean_variable = True
+character_variable = 'a'
+string_variable = 'string'
+list_variable = ['l', 'i', 's', 't']
+tuple_variable = ('t', 'u', 'p', 'l', 'e')
+dictionary_variable = {'type': 'dictionary', 'use': 'key value pair'}
+set_variable = {'this', 'is', 'a', 'set'}
+frozenset_variable = frozenset({'this', 'is', 'a', 'frozenset'})
+data_time_variable = datetime.datetime.now()
+
+print('This is an integer variable:', integer_variable)
+print('This is a float variable:', float_variable)
+print('This is a boolean variable:', boolean_variable)
+print('This is a character variable:', character_variable)
+print('This is a string variable:', string_variable)
+print('This is a list variable:', list_variable)
+print('This is a tuple variable:', tuple_variable)
+print('This is a dictionary variable:', dictionary_variable)
+print('This is a set variable:', set_variable)
+print('this is a frozenset variable:', frozenset_variable)
+print('This is a date/time variable:', data_time_variable)
