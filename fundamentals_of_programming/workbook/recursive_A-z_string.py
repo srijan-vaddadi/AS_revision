719209a4a4fb4cea2178c7e83e6641f6fd4efcca
Srijan <srijan.vaddadi@gmail.com>
2022-05-30 16:31:40 +0100

    recursive

--- fundamentals_of_programming/workbook/recursive_A-z_string.py
+++ fundamentals_of_programming/workbook/recursive_A-z_string.py
@@ -1,1 +1,20 @@
-def high_letter_func(n):
+'''def recsub(num):
+    if num>0:
+        recsub(num - 1)
+    print(num)
+
+recsub(100)
+
+'''
+
+
+def highLet(let, word):
+    x = let
+    if let < word[0]:
+        x = word[0]
+    if len(word) > 1:
+        return highLet(x, word[1:])
+
+print(highLet('a', "My test string"))
+
+# def high_letter(n):
